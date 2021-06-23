8719d4f383be23d03934543d34aafc14fede7e9a
Hadi Golkarian <HadiGolkarian@gmail.com>
2021-06-24 00:01:56 +0430

    added in memory web api with a small example data

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { InMemoryDataService } from './dataServices/in-memory-data.service';
 
 @NgModule({
+  imports: [
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
+  ],
   providers: [],
 })
 export class CoreModule {}
