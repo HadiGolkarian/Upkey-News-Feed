6b2ed8cf6bba7cafc95182b05abb3f9d9591134d
Hadi Golkarian <HadiGolkarian@gmail.com>
2021-06-23 13:36:34 +0430

    added main modules

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '@shared/shared.module';
+import { LayoutRoutingModule } from './layout-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [SharedModule, LayoutRoutingModule],
+})
+export class LayoutModule {}
