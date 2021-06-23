24ce7aa6693d38b80596642fc4e0113fc03c1a1f
Hadi Golkarian <HadiGolkarian@gmail.com>
2021-06-23 13:48:27 +0430

    added dashboard component added lazyloading for layout module

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LayoutRoutingModule { }
+export class LayoutRoutingModule {}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '@shared/shared.module';
 import { LayoutRoutingModule } from './layout-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [SharedModule, LayoutRoutingModule],
 })
 export class LayoutModule {}
