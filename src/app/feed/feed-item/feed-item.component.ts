d0a69bce8adc5454895406ded6d7e91433e6d115
Hadi Golkarian <HadiGolkarian@gmail.com>
2021-06-26 12:44:01 +0430

    fixed feed items list ui added fontawesome

--- src/app/feed/feed-item/feed-item.component.ts
+++ src/app/feed/feed-item/feed-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FeedType } from '@app/abstractions/feeds/feed-type.enum';
 import { Feed } from '@app/abstractions/feeds/feed.model';
 
 @Component({
@@ -10,6 +11,32 @@ export class FeedItemComponent implements OnInit {
   @Input() feed: Feed;
 
   constructor() {}
+
+  getFeedDescription(): string {
+    switch (this.feed.type) {
+      case FeedType.addedFriend:
+        return `${this.feed.owner.firstName} ${this.feed.owner.lastName} added ${this.feed.subject.firstName} ${this.feed.subject.lastName} as a firend.`;
+      case FeedType.like:
+        return `${this.feed.owner.firstName} ${this.feed.owner.lastName} liked ${this.feed.subject.firstName} ${this.feed.subject.lastName} post.`;
+      case FeedType.upload:
+        return `${this.feed.owner.firstName} ${this.feed.owner.lastName} uploaded a new image.`;
+      default:
+        return '';
+    }
+  }
+
+  getIconClass(): string {
+    switch (this.feed.type) {
+      case FeedType.addedFriend:
+        return 'fas fa-user-plus';
+      case FeedType.like:
+        return 'fas fa-thumbs-up';
+      case FeedType.upload:
+        return 'fas fa-cloud-upload-alt';
+      default:
+        return '';
+    }
+  }
 
   ngOnInit(): void {}
 }
