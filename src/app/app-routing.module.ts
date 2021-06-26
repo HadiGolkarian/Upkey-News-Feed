3a6a652bf382dc8a1bc0f418e75e07b71d40e545
Hadi Golkarian <HadiGolkarian@gmail.com>
2021-06-26 17:15:52 +0430

    added feed cover

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    // lazy load feature module
     loadChildren: () =>
       import('./layout/layout.module').then((m) => m.LayoutModule),
   },
